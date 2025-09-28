e13c191cef25982e988e55bf22783c4f6dd3a76f
ashwin.g <ashwin.g@greyorange.com>
2025-09-28 20:48:03 +0530

    get_api

--- helper/apiHelper.py
+++ helper/apiHelper.py
@@ -1,8 +1,25 @@
 import requests
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
 
 class ApiHelper:
 
-    def get_last_name(self):
+    def __init__(self):
+        self.url = os.getenv("API_URL")
+
+    def get_name(self):
+        user_list = []
+        response = requests.get(f" {self.url}/get_users")
+        data = response.json()
+        for obj in data:
+            user_list.append(obj["name"])
+        return user_list
+
+
+
+
 
 
 
